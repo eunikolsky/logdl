4b008d95d2463d7dc73d7e04bd94c9a5c3cbfac2
Eugene Nikolsky <eunikolsky@gmail.com>
2019-07-24 20:57:34 -0700

    Print available links

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Time.Calendar
 import           Data.Time.Clock
 import           Network.HTTP.Client
+import           Text.HTML.TagSoup
+import           Text.StringLike (StringLike)
 
 -- |Returns today's day of the month.
 today :: IO Int
 today = extractDay . toGregorian . utctDay <$> getCurrentTime
   where extractDay (_, _, day) = day
+
+-- |Extracts the @href@ links from the @a@ tags.
+extractLinks :: (Show a, StringLike a) => [Tag a] -> [a]
+extractLinks = fmap (fromAttrib "href") . filter (isTagOpenName "a")
 
 main :: IO ()
 main = do
@@ -17,4 +25,6 @@ main = do
   request <- parseRequest "http://localhost:50000/index.html"
   response <- httpLbs request manager
 
-  L8.putStrLn $ responseBody response
+  let tags = parseTags $ responseBody response
+
+  print $ extractLinks tags
