cff30064d69934f3acfb7e0cb8ac63d926d8a8e3
Eugene Nikolsky <eunikolsky@gmail.com>
2019-07-24 20:25:04 -0700

    Get today's day of the month

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.Time.Calendar
+import           Data.Time.Clock
 import           Network.HTTP.Client
+
+-- |Returns today's day of the month.
+today :: IO Int
+today = extractDay . toGregorian . utctDay <$> getCurrentTime
+  where extractDay (_, _, day) = day
 
 main :: IO ()
 main = do
