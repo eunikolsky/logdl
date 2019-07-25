93068d59745aa395beb07f4a90a342aa43fa9b15
Eugene Nikolsky <eunikolsky@gmail.com>
2019-07-24 21:48:27 -0700

    Filter out today's log link

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import           Control.Applicative (liftA2)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Time.Calendar
 import           Data.Time.Clock
@@ -21,6 +22,13 @@ extractLinks = fmap (fromAttrib "href") . filter (isTagOpenName "a")
 isDeleteLink :: L8.ByteString -> Bool
 isDeleteLink = ("!DEL!" `L8.isPrefixOf`)
 
+isTodaysLink :: Int -> L8.ByteString -> Bool
+isTodaysLink today = (== (L8.pack . show $ today) <> ".txt")
+
+-- Source: https://stackoverflow.com/questions/5710078/in-haskell-performing-and-and-or-for-boolean-functions
+f_or :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+f_or = liftA2 (||)
+
 main :: IO ()
 main = do
   manager <- newManager defaultManagerSettings
@@ -31,4 +39,4 @@ main = do
   let tags = parseTags $ responseBody response
   today <- getToday
 
-  print $ filter (not . isDeleteLink) $ extractLinks tags
+  print $ filter (not . (isDeleteLink `f_or` isTodaysLink today)) $ extractLinks tags
