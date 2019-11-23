ede9d1aa2d3d0b085cb96636311a1142d1aee3d4
Eugene Nikolsky <eunikolsky@gmail.com>
2019-11-23 22:17:18 +0200

    Use `Reader` to pass the `Config`

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
@@ -6,8 +7,10 @@ import           Action
 import           RemoteFile
 
 import           Control.Monad (forM_)
+import           Control.Monad.Reader
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.Functor ((<&>))
 import           Data.List
 import           Data.Maybe
 import           Data.Time.Calendar
@@ -20,8 +23,11 @@ import           System.Directory
 import           Text.HTML.TagSoup
 import           Text.StringLike (StringLike)
 
-urlForFile :: Config -> String -> String
-urlForFile config path = mconcat ["http://", cfgHost config, ":", show . cfgPort $ config, "/", path]
+urlForFile :: MonadReader Config m => String -> m String
+urlForFile path = do
+  host <- asks cfgHost
+  port <- asks cfgPort
+  return . mconcat $ ["http://", host, ":", show port, "/", path]
 
 -- |Returns today's day of the month in the local timezone.
 getToday :: IO Int
@@ -34,54 +40,58 @@ getToday = extractDay . toGregorian . localDay <$> localTime
 extractLinks :: (Show a, StringLike a) => [Tag a] -> [a]
 extractLinks = fmap (fromAttrib "href") . filter (isTagOpenName "a")
 
-downloadFile :: Config -> Manager -> RemoteFile -> IO ()
-downloadFile config manager file = do
-  let url = urlForFile config . remoteName $ file
+downloadFile :: Manager -> RemoteFile -> ReaderT Config IO ()
+downloadFile manager file = do
+  url <- urlForFile . remoteName $ file
   let localFilename = localName file
-  putStrLn $ "Downloading " <> url
+  lift $ putStrLn $ "Downloading " <> url
 
-  fileExists <- doesPathExist localFilename
-  if fileExists
-  then
-    putStrLn $ mconcat ["File ", localFilename, " already exists. Not overwriting."]
-  else do
-    response <- parseRequest url >>= flip httpLbs manager
-    L8.writeFile localFilename $ responseBody response
-
-    let modDateStr = fmap snd $ find ((== "Last-Modified") . fst) $ responseHeaders response
-    let parsedUTCTime = fmap B8.unpack modDateStr >>= parseTimeM False defaultTimeLocale rfc822DateFormat :: Maybe UTCTime
-    case parsedUTCTime of
-      Just parsedUTCTime -> setModificationTime localFilename parsedUTCTime
-      Nothing -> putStrLn "Can't determine mod time from the response"
-
-deleteFile :: Config -> Manager -> RemoteFile -> IO ()
-deleteFile config manager file = do
-  let url = urlForFile config . ("!DEL!" ++) . remoteName $ file
-  putStrLn $ "Removing " <> remoteName file
-
-  parseRequest url >>= flip httpLbs manager
-  return ()
-
-run :: Config -> IO ()
-run config = do
-  manager <- newManager defaultManagerSettings
-
-  request <- parseRequest $ urlForFile config ""
-  response <- httpLbs request manager
-
-  let tags = parseTags $ responseBody response
-  today <- getToday
-
-  let files = mapMaybe (makeRemoteFile today . L8.unpack) $ extractLinks tags
-  forM_ files (actionF config manager)
+  lift $ saveFile url localFilename
 
   where
-    actionF = case cfgAction config of
-      Fetch -> downloadFile
-      Delete -> deleteFile
+    saveFile :: String -> FilePath -> IO ()
+    saveFile url localFilename = do
+      fileExists <- doesPathExist localFilename
+      if fileExists
+      then
+        putStrLn $ mconcat ["File ", localFilename, " already exists. Not overwriting."]
+      else do
+        response <- parseRequest url >>= flip httpLbs manager
+        L8.writeFile localFilename $ responseBody response
+
+        let modDateStr = fmap snd $ find ((== "Last-Modified") . fst) $ responseHeaders response
+        let parsedUTCTime = fmap B8.unpack modDateStr >>= parseTimeM False defaultTimeLocale rfc822DateFormat :: Maybe UTCTime
+        case parsedUTCTime of
+          Just parsedUTCTime -> setModificationTime localFilename parsedUTCTime
+          Nothing -> putStrLn "Can't determine mod time from the response"
+
+deleteFile :: Manager -> RemoteFile -> ReaderT Config IO ()
+deleteFile manager file = do
+  url <- urlForFile . ("!DEL!" ++) . remoteName $ file
+  liftIO $ putStrLn $ "Removing " <> remoteName file
+
+  liftIO $ parseRequest url >>= flip httpLbs manager
+  return ()
+
+run :: ReaderT Config IO ()
+run = do
+  manager <- liftIO $ newManager defaultManagerSettings
+
+  request <- parseRequest <$> urlForFile ""
+  response <- liftIO $ flip httpLbs manager <$> request
+
+  tags <- lift $ parseTags . responseBody <$> response
+  today <- lift getToday
+
+  actionF <- asks cfgAction <&> \action -> case action of
+    Fetch -> downloadFile
+    Delete -> deleteFile
+
+  let files = mapMaybe (makeRemoteFile today . L8.unpack) $ extractLinks tags
+  forM_ files (actionF manager)
 
 main :: IO ()
-main = run =<< execParser opts
+main = runReaderT run =<< execParser opts
   where
     opts = info (configP <**> helper)
       ( fullDesc
