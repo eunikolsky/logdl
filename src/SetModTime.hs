540a368f2ca2db7836549820888d71152bdc53f1
Eugene Nikolsky <eunikolsky@gmail.com>
2019-11-24 21:27:10 +0200

    Implement `wrongTimeFormatErrors` function

--- src/SetModTime.hs
+++ src/SetModTime.hs
@@ -2,6 +2,7 @@ module SetModTime
   ( SetModTimeError(..)
   , SetModTimeResult(..)
   , noTimeHeaderErrors
+  , wrongTimeFormatErrors
   ) where
 
 import RemoteFile (Filename)
@@ -19,13 +20,23 @@ data SetModTimeResult = SetModTimeResult
 data SetModTimeError
   -- |The server didn't provide the modification time (@Last-Modified@ header).
   = NoTimeHeader
-  | WrongTimeFormat
+  -- |The provided time string couldn't be parsed.
+  | WrongTimeFormat String
   deriving (Show)
 
--- |Extracts the filenames for which the errors were @NoTimeHeader@.
+-- |Extracts the filenames for which the errors are @NoTimeHeader@.
 noTimeHeaderErrors :: [SetModTimeResult] -> [Filename]
 noTimeHeaderErrors = mapMaybe toError
   where
     toError :: SetModTimeResult -> Maybe Filename
     toError (SetModTimeResult f (Left NoTimeHeader)) = Just f
+    toError _ = Nothing
+
+-- |Extracts the filenames and corresponding time strings for which the
+-- |errors are @WrongTimeFormat@.
+wrongTimeFormatErrors :: [SetModTimeResult] -> [(Filename, String)]
+wrongTimeFormatErrors = mapMaybe toError
+  where
+    toError :: SetModTimeResult -> Maybe (Filename, String)
+    toError (SetModTimeResult f (Left (WrongTimeFormat t))) = Just (f, t)
     toError _ = Nothing
--- test/SetModTimeSpec.hs
+++ test/SetModTimeSpec.hs
@@ -5,7 +5,7 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "noTimeHeaderErrors" $ do
-    it "returns filenames for the error" $ do
+    it "returns filenames for the NoTimeHeader error" $ do
       let { input =
         [ SetModTimeResult "foo" $ Left NoTimeHeader
         , SetModTimeResult "1.txt" $ Left NoTimeHeader
@@ -23,8 +23,36 @@ main = hspec $ do
 
     it "ignores WrongTimeFormat errors" $ do
       let { input =
-        [ SetModTimeResult "baz" $ Left WrongTimeFormat
-        , SetModTimeResult "#" $ Left WrongTimeFormat
+        [ SetModTimeResult "baz" $ Left $ WrongTimeFormat ""
+        , SetModTimeResult "#" $ Left $ WrongTimeFormat ""
         ]
       }
       noTimeHeaderErrors input `shouldBe` []
+
+  describe "wrongTimeFormatErrors" $ do
+    it "returns filenames and time strings for the WrongTimeFormat error" $ do
+      let { input =
+        [ SetModTimeResult "foo" $ Left (WrongTimeFormat "foo!")
+        , SetModTimeResult "1.txt" $ Left (WrongTimeFormat "20200101 000159")
+        ]
+      }
+      wrongTimeFormatErrors input `shouldBe`
+        [ ("foo", "foo!")
+        , ("1.txt", "20200101 000159")
+        ]
+
+    it "ignores successful filenames" $ do
+      let { input =
+        [ SetModTimeResult "bar" $ Right ()
+        , SetModTimeResult "0" $ Right ()
+        ]
+      }
+      wrongTimeFormatErrors input `shouldBe` []
+
+    it "ignores NoTimeHeader errors" $ do
+      let { input =
+        [ SetModTimeResult "baz" $ Left NoTimeHeader
+        , SetModTimeResult "#" $ Left NoTimeHeader
+        ]
+      }
+      wrongTimeFormatErrors input `shouldBe` []
