e489a0733bf77e7c561c3dc79852abb8b21a5c79
Eugene Nikolsky <e@egeek.me>
2022-04-09 08:50:24 +0300

    Print a message once when waiting for server to (dis)appear
    This is an elegant solution of using a `StateT Bool IO` to track whether
    it's the first fail. This monad wraps the `IO` that is forced by
    `catch`. There is no need to change/extend `whileM`.

--- src/Wait.hs
+++ src/Wait.hs
@@ -15,6 +15,7 @@ import           PreludeExt
 import           Control.Concurrent (threadDelay)
 import           Control.Exception (catch)
 import           Control.Monad.Reader
+import           Control.Monad.State.Strict
 import           Data.Functor (($>))
 import           Network.HTTP.Client
 
@@ -29,16 +30,29 @@ waitForAppearance :: Manager -> ReaderT Config IO ()
 waitForAppearance manager = do
   url <- urlForFile ""
   request <- makeHEAD <$> parseRequest url
-  liftIO $ whileM $ (httpNoBody request manager $> False)
-    `catch` (\(_ :: HttpException) -> threadDelay 1_000_000 $> True)
+  liftIO . flip evalStateT True . whileM $ do
+    firstFail <- get
+    put False
+    liftIO $ (httpNoBody request manager $> False)
+      `catch` \(_ :: HttpException) -> do
+        when firstFail $ putStrLn "Waiting for server…"
+        threadDelay 1_000_000
+        pure True
 
 -- | Wait until the requested server stops responding to requests.
 waitForDisappearance :: Manager -> ReaderT Config IO ()
 waitForDisappearance manager = do
   url <- urlForFile ""
   request <- makeHEAD <$> parseRequest url
-  liftIO $ whileM $ (httpNoBody request manager >> threadDelay 1_000_000 $> True)
-    `catch` (\(_ :: HttpException) -> pure False)
+  liftIO . flip evalStateT True . whileM $ do
+    firstFail <- get
+    put False
+    liftIO $ do
+        httpNoBody request manager
+        when firstFail $ putStrLn "Waiting for server to go away…"
+        threadDelay 1_000_000
+        pure True
+      `catch` \(_ :: HttpException) -> pure False
 
 makeHEAD :: Request -> Request
 makeHEAD req = req { method = "HEAD" }
