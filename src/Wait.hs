9672f69f7c01aa8509db4b719bcbc0b698050790
Eugene Nikolsky <e@egeek.me>
2022-04-09 18:09:10 +0300

    Remove code duplication in waiting functions

--- src/Wait.hs
+++ src/Wait.hs
@@ -27,31 +27,24 @@ urlForFile path = do
 
 -- | Wait until the requested server starts responding to requests.
 waitForAppearance :: Manager -> ReaderT Config IO ()
-waitForAppearance manager = do
-  url <- urlForFile ""
-  request <- makeHEAD <$> parseRequest url
-  liftIO . flip evalStateT True $ untilM
-    post
-    (liftIO $ isServerPresent manager request)
-
-  where
-    post :: StateT Bool IO ()
-    post = do
-      firstFail <- get
-      put False
-
-      liftIO $ do
-        when firstFail $ putStrLn "Waiting for server…"
-        threadDelay 1_000_000
+waitForAppearance = wait "Waiting for server…" id
 
 -- | Wait until the requested server stops responding to requests.
 waitForDisappearance :: Manager -> ReaderT Config IO ()
-waitForDisappearance manager = do
+waitForDisappearance = wait "Waiting for server to go away…" not
+
+-- | Internal helper with the template for @waitForAppearance@ and
+-- @waitForDisappearance@ functions.
+wait :: String      -- ^ message to display when we're waiting for the server
+  -> (Bool -> Bool) -- ^ modifier of the @isServerPresent@ response
+  -> Manager        -- ^ HTTP manager
+  -> ReaderT Config IO ()
+wait message serverPresentMod manager = do
   url <- urlForFile ""
   request <- makeHEAD <$> parseRequest url
   liftIO . flip evalStateT True $ untilM
     post
-    (liftIO . fmap not $ isServerPresent manager request)
+    (liftIO . fmap serverPresentMod $ isServerPresent manager request)
 
   where
     post :: StateT Bool IO ()
@@ -60,7 +53,7 @@ waitForDisappearance manager = do
       put False
 
       liftIO $ do
-        when firstFail $ putStrLn "Waiting for server to go away…"
+        when firstFail $ putStrLn message
         threadDelay 1_000_000
 
 isServerPresent :: Manager -> Request -> IO Bool
